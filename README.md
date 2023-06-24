59d427bd60633370fb5954f84733c9cbcdb1f939
YennhiDUT <91811893+YennhiDUT@users.noreply.github.com>
2023-06-24 21:14:30 +0700

    fix

---
+++ README.md
@@ -0,0 +1,7 @@
+# Dashboard with multiple theme
+
+- Build dashboard with multiple theme using: reactjs, redux, apexcharts.
+
+- <a href="https://dashboard-admin-react.netlify.app/" target="_blank">Preview App <a>
+
+- Contact me: tranvansu2001@gmail.com
---
+++ src/index.js
@@ -0,0 +1,26 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+import { createStore } from "redux";
+
+import { Provider } from "react-redux";
+
+import rootReducer from "./redux/reducers";
+
+import "./assets/boxicons-2.0.7/css/boxicons.min.css";
+import "./assets/css/grid.css";
+import "./assets/css/theme.css";
+import "./assets/css/index.css";
+
+import Layout from "./components/Layout/Layout";
+
+const store = createStore(rootReducer);
+
+document.title = "Dashboard Admin React";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <Provider store={store}>
+    <Layout />
+  </Provider>
+);
