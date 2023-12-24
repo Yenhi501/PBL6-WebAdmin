4dd38e4b504f883e3a4b24e98b427df28fe5a78e
blognn_287 <83860336+pikadragonelse@users.noreply.github.com>
2023-12-24 10:31:51 +0700

    feat: modify layout

--- src/index.tsx
+++ src/index.tsx
@@ -3,16 +3,23 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
 import { App } from './App';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import rootReducer from './redux/reducers';
 
 document.title = 'Dashboard Admin React';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
 );
+
+const store = createStore(rootReducer);
 root.render(
   <React.StrictMode>
     <Router>
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </Router>
   </React.StrictMode>,
 );
